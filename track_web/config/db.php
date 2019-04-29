85ffdb891597b82664c2b30bbb4d5b2637a82497
Georgy Smirnov <georgy.smirnov.valeryevich@gmail.com>
2019-04-29 13:31:56 +0500

    Initial commit

---
+++ track_web/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=geo_track',
+    'username' => 'webuser',
+    'password' => 'webpass',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
